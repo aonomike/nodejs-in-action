407feb4c25ef7a4512149862c40da66a06d46733
aonomike <aonomike@gmail.com>
2016-12-22 21:10:01 +0300

    Added bug fixes to make the application run

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # nodejs-in-action
-This is a project based on book node.js in action by Mike Caltelon et al. It is meant to grow my understanding of node.js. Let the journey begin.
+This is a project based on book node.js in action by Mike Caltelon et al. It is meant to grow my understanding of node.js. The tutorial is based on chatroom application that enables people to chat realtime. Run the application by typing server.js in chatroom root folder.
--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 var http = require('http');
 var fs = require('fs');
-var path = reqire('path');
+var path = require('path');
 var mime = require('mime');
 var cache = {};
 
+//handle 404 errors for files that don't exist
 function send404(response) {
 	response.writeHead(404, {'Content-Type': 'text/plain'});
 	response.write('Error 404: Resource not found');
 	response.end();
 }
 
+//serve file data
 function sendFile(response, filePath, fileContents) {
 	response.writeHead(200, {contentType : mime.lookup(path.basename(filePath))});
 	response.end(fileContents);
@@ -17,17 +19,18 @@ function sendFile(response, filePath, fileContents) {
 
 //serving static files
 function serveStatic(response, cache, absPath) {
-	if (cache(absPath)) {
-		sendFile(response, absPath, cache[absPath]);		
-	} else {
+	if (cache[absPath]) {
+		sendFile(response, absPath, cache[absPath]);
+	}
+ 	else {
 		fs.exists(absPath, function(exists){
 			if(exists) {
-				fs.readFile(absPath, functon(err,data){
+				fs.readFile(absPath, function(err, data){
 					if(err) {
 						send404(response);
 					}
 					else {
-						cache[absPath = data;
+						cache[absPath] = data;
 						sendFile(response, absPath, data);
 					}
 				});
@@ -38,3 +41,22 @@ function serveStatic(response, cache, absPath) {
 	}
 }
 
+//logic to create http server
+var server = http.createServer(function(request, response) {
+	var filePath = false;
+	var absPath = './';
+	if (request.url == '/') {
+		filePath = 'public/index.html';
+	}
+	else {
+		filePath = 'public' + request.url;
+	}
+	absPath = absPath + filePath;
+	serveStatic(response, cache, absPath);
+
+});
+
+//start server
+server.listen(3000, function() {
+	console.log("server listening on port 3000");
+});
